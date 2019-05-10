ecf1b53a746a7d62ab5bb72b2da149119f8260e0
Brittany F <brittfranklin92@gmail.com>
2019-05-10 18:33:18 -0400

    Update to fs

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,7 +3,7 @@ var path = require('path');
 
 module.exports = function(app) {
   app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '../public/html/home.html'));
+    res.sendFile(path.join(__dirname, '../public/html/index.html'));
   });
 
   app.get('/jobs', function(req, res) {
